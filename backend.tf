4df9e05c51fd7155a63cd914e7ca4525af0cb4f3
Aayush Meshram <aayush.meshram@ZL-BLR-MAC529.local>
2024-03-22 15:32:31 +0530

    added client secret

--- backend.tf
+++ backend.tf
@@ -11,7 +11,7 @@ terraform {
     storage_account_name = "satfstatetraining"
     container_name = "state"
     key = "terraform.tfstate"  # Name of the state file
-    # skip_provider_registration = "true"
+    skip_provider_registration = "true"
     # access_key = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-03-21T14:27:09Z&st=2024-03-21T06:27:09Z&spr=https&sig=aVxs41LqYsc9KLjWAIr42QHLn%2FfNWzsJuqUdvW8Q2Ig%3D"  # Replace with key or SAS token
 
     # Optional: Configure encryption for additional security
