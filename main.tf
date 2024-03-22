ae3cdbe3da975cc3a793eb0b1ef39e9c897049e8
Aayush Meshram <aayush.meshram@ZL-BLR-MAC529.local>
2024-03-22 16:49:07 +0530

    added client secret

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_public_ip" "pip-vm-win" {
   name                = "pip-vm-training-win-0001"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_key_vault" "kv" {
