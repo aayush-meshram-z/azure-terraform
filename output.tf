054aa5f84903affa9dd235beaf8a2ef131269b12
Aayush Meshram <aayush.meshram@ZL-BLR-MAC529.local>
2024-03-27 16:38:30 +0530

    Decommissioning Windows VM and creating Linux VM

--- output.tf
+++ output.tf
@@ -3,10 +3,10 @@ output "resource_group_name" {
 }
 
 output "public_ip_address" {
-  value = azurerm_windows_virtual_machine.vm.public_ip_address
+  value = azurerm_linux_virtual_machine.linux_vm.public_ip_address
 }
 
-output "admin_password" {
-  sensitive = true
-  value     = azurerm_windows_virtual_machine.vm.admin_password
-}
+# output "admin_password" {
+#   sensitive = true
+#   value     = azurerm_windows_virtual_machine.vm.admin_password
+# }
